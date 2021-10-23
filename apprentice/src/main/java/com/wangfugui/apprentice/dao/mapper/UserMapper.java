ade715a626ccc6fa770ea9f4031c0007bcdb19b8
masiyi <masiyi163163@163.com>
2021-10-23 13:53:34 +0800

    构建框架

---
+++ apprentice/src/main/java/com/wangfugui/apprentice/dao/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.wangfugui.apprentice.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wangfugui.apprentice.dao.domain.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
