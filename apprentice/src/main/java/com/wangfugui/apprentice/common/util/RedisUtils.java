03b693029f3dd75a805030e2e18d2aa02fb41379
MrFugui <masiyi163163@163.com>
2021-11-20 15:06:25 +0800

    集成验证码

--- apprentice/src/main/java/com/wangfugui/apprentice/common/util/RedisUtils.java
+++ apprentice/src/main/java/com/wangfugui/apprentice/common/util/RedisUtils.java
@@ -1,7 +1,9 @@
 package com.wangfugui.apprentice.common.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,16 @@ import java.util.concurrent.TimeUnit;
 public class RedisUtils {
 
     @Autowired
-    private  RedisTemplate redisTemplate;
+    @Qualifier("getRedisTemplate")
+    private RedisTemplate redisTemplate;
 
+    @Autowired
+    @Qualifier("getStringRedisTemplate")
+    private StringRedisTemplate stringRedisTemplate;
 
-    /** 设置键值
+    /**
+     * 设置键值
+     *
      * @Param: [key, value]
      * @return: boolean
      * @Author: MaSiyi
@@ -37,13 +45,51 @@ public class RedisUtils {
     }
 
     /**
+     * 设置键值(string)
+     *
+     * @Param: [key, value]
+     * @return: boolean
+     * @Author: MaSiyi
+     * @Date: 2021/11/20
+     */
+    public boolean setStr(final String key, String value) {
+        boolean result = false;
+        try {
+            ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+            operations.set(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    /**
+     * 设置键值(string)
+     *
+     * @Param: [key, value]
+     * @return: boolean
+     * @Author: MaSiyi
+     * @Date: 2021/11/20
+     */
+    public boolean setStrEx(final String key, String value,long timeout, TimeUnit unit) {
+        boolean result = false;
+        try {
+            stringRedisTemplate.opsForValue().set(key, value, timeout, unit);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
      * 写入缓存设置失效时间
      *
      * @param key
      * @param value
      * @return
      */
-    public boolean set(final String key, Object value, Long expireTime) {
+    public boolean setEx(final String key, Object value, Long expireTime) {
         boolean result = false;
         try {
             ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
@@ -54,6 +100,21 @@ public class RedisUtils {
             e.printStackTrace();
         }
         return result;
+    }
+
+    /**
+     * 设置key-value
+     * <p>
+     * 注: 若已存在相同的key, 那么原来的key-value会被丢弃
+     *
+     * @param key     key
+     * @param value   key对应的value
+     * @param timeout 过时时长
+     * @param unit    timeout的单位
+     * @date 2020/3/8 15:40:59
+     */
+    public void setEx(String key, String value, long timeout, TimeUnit unit) {
+        redisTemplate.opsForValue().set(key, value, timeout, unit);
     }
 
     /**
@@ -82,6 +143,28 @@ public class RedisUtils {
      */
     public boolean exists(final String key) {
         return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * 根据key，获取到对应的value值
+     *
+     * @param key key-value对应的key
+     * @return 该key对应的值。
+     * 注: 若key不存在， 则返回null。
+     */
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 根据key，获取到对应的value(string)值
+     *
+     * @param key key-value对应的key
+     * @return 该key对应的值。
+     * 注: 若key不存在， 则返回null。
+     */
+    public String getStr(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
     }
 
 
