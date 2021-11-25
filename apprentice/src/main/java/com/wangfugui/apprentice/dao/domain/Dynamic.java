0d34132da19f779003b5a2504e9c2a233fb40560
MrFugui <masiyi163163@163.com>
2021-11-25 11:38:33 +0800

    添加ElasticSearch功能

--- apprentice/src/main/java/com/wangfugui/apprentice/dao/domain/Dynamic.java
+++ apprentice/src/main/java/com/wangfugui/apprentice/dao/domain/Dynamic.java
@@ -1,9 +1,13 @@
 package com.wangfugui.apprentice.dao.domain;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -14,6 +18,9 @@ import io.swagger.annotations.ApiModelProperty;
  * @since 2021-11-23
  */
 @ApiModel(value = "Dynamic对象", description = "动态表")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Dynamic implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -45,91 +52,8 @@ public class Dynamic implements Serializable {
     @ApiModelProperty("地址")
     private String address;
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
+    public Dynamic( String dynamicText,Integer id) {
         this.id = id;
-    }
-
-    public String getDynamicText() {
-        return dynamicText;
-    }
-
-    public void setDynamicText(String dynamicText) {
         this.dynamicText = dynamicText;
-    }
-
-    public Integer getFileId() {
-        return fileId;
-    }
-
-    public void setFileId(Integer fileId) {
-        this.fileId = fileId;
-    }
-
-    public Integer getGoodNumber() {
-        return goodNumber;
-    }
-
-    public void setGoodNumber(Integer goodNumber) {
-        this.goodNumber = goodNumber;
-    }
-
-    public Integer getTalkNumber() {
-        return talkNumber;
-    }
-
-    public void setTalkNumber(Integer talkNumber) {
-        this.talkNumber = talkNumber;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public Boolean getDeleteFlag() {
-        return deleteFlag;
-    }
-
-    public void setDeleteFlag(Boolean deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
-
-    public Integer getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(Integer createUser) {
-        this.createUser = createUser;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Dynamic{" +
-        "id=" + id +
-        ", dynamicText=" + dynamicText +
-        ", fileId=" + fileId +
-        ", goodNumber=" + goodNumber +
-        ", talkNumber=" + talkNumber +
-        ", createTime=" + createTime +
-        ", deleteFlag=" + deleteFlag +
-        ", createUser=" + createUser +
-        ", address=" + address +
-        "}";
     }
 }
